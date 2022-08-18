bf0ea75d77bc3995d694efeead73df5b14f9102e
Victor Granda <victorgranda1992@gmail.com>
2022-08-18 20:40:39 +0200

    Create Calculadora.js

---
+++ Calculadora.js
@@ -0,0 +1,29 @@
+//calculadora: 2 parametros, visualiza suma, resta, multiplicación y división.
+// 3 decimales
+let resultadoTotal =[];
+function calculadora(num1,num2){
+    let resultados = []; //store datos
+    //check NAN
+    if (isNaN(num1 * num2) || num1 === ' ' || num2 === ' ' || (num1 === '' && num2 === '')) { 
+         return console.log("error, los datos introducidos no son números")
+       }
+    if (num1 === 0 ){
+        let raizcua = Math.sqrt(num2);
+        resultados.push(raizcua);
+        console.log(`La raiz cuadrada de ${num2} es ${raizcua}`);
+    }
+    if (num2 === 0){
+        let raizcua = Math.sqrt(num1);
+        resultados.push(raizcua);
+        console.log(`La raiz cuadrada de ${num1} es ${raizcua}`);
+    }
+    else{
+    let suma = (num1 + num2);
+    let resta = (num1 - num2);
+    let multiplicación = (num1 * num2);
+    let división = (num1 / num2).toFixed(3);
+    resultados.push(`Los resultados obtenidos són: \n ${num1} + ${num2} = ${suma} \n ${num1} - ${num2} = ${resta}\n ${num1} * ${num2} = ${multiplicación}\n ${num1} / ${num2} = ${división}`);
+    console.log(resultados)
+    }
+}
+calculadora(5,8)
