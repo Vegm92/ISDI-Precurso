dfcd89619d9e0016d9d0846c377e3eb71be35883
Victor Granda <victorgranda1992@gmail.com>
2022-10-04 11:04:22 +0200

    Update calculatorPro.js

--- calculatorPro.js
+++ calculatorPro.js
@@ -1,11 +1,9 @@
 let result = [];
 let promptNumbers = [];
 
-
-
 const getNumbers = () => { 
     let givenNumbers = [];
-    let number;
+    let number = 0;
     do{
         number =  Number(prompt ("Añade número y pulsa aceptar para seguir añadiendo.\n Si solo añades un numero, se calculará la raiz cuadrada."));
         if (isNaN(number)){
@@ -28,10 +26,15 @@ const getNumbers = () => {
 
 const calcular = () => {
     let calculos = [];
-    if(promptNumbers.length < 2){
+    if(promptNumbers.length < 2 && promptNumbers !== NaN){
         const raizCuadrada = Math.sqrt(promptNumbers[0]);
-        calculos.push(`Solo se calculará su raiz cuadrada: ${raizCuadrada.toFixed(3).replace(/[.,]000$/, "")}`);
-        return calculos;
+        if(isNaN(raizCuadrada)){
+            return calculos
+        } else{
+            calculos.push(`Solo se calculará su raiz cuadrada: ${raizCuadrada.toFixed(3).replace(/[.,]000$/, "")}`);
+            return calculos;
+
+            }
     } else {
      
     const suma = promptNumbers.reduce((sum, num) => sum + num);
@@ -43,26 +46,26 @@ const calcular = () => {
     multiplicación: ${multiplicación.toFixed(3).replace(/[.,]000$/, "")}, división: ${división.toFixed(3).replace(/[.,]000$/, "")}`); //replace cambia los decimales por un espacio si no se usan
     
     return calculos;      
-    }
-    
+    }    
 }
+
 const reCalcular = () => {
     if(confirm("¿Quieres calcular de nuevo?")){
+        promptNumbers = [];
         return mainCalculator();
     } else {
         despedida();
     }
 
 }
-const  mostrarResultados = () => {
-    debugger;
 
+const  mostrarResultados = () => {
     do{
-        if(promptNumbers.length < 2 && result.length > 1){
+        if(promptNumbers.length < 2 && result.length >= 1 && promptNumbers !== NaN){
             alert(`El número introducido es: ${promptNumbers}: ${result}`)
             
         }
-        if(promptNumbers.length > 1 && result.length > 1){
+        if(promptNumbers.length > 1 && result.length >= 1 && result !== NaN){
 
             alert(`Los números introducidos són: ${promptNumbers}, y los calculos realizados: ${result}`)
     
@@ -73,6 +76,7 @@ const  mostrarResultados = () => {
     }
     reCalcular();
 }
+
 const despedida = () =>{
    return alert(`Gracias por usar esta calculadora!`)
 }
@@ -82,4 +86,5 @@ const mainCalculator = () =>{
     result = calcular();
     mostrarResultados();
 }
+
 mainCalculator();
